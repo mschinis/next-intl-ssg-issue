011a94019b0d2e84a9cc8cbf4bf2163afb49c4ce
Michael Schinis <mschinis@gmail.com>
2024-11-01 13:44:17 +0200

    chore: Add check for middleware too

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,13 @@
 import createMiddleware from 'next-intl/middleware';
 import {routing} from './i18n/routing';
 
-export default createMiddleware(routing);
+export default function middleware() {
+    if (process.env.NEXT_PUBLIC_USE_NEXT_INTL !== 'true') {
+        return createMiddleware(routing)
+    }
+
+    return;
+};
 
 export const config = {
     // Match only internationalized pathnames
